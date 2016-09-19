d4b67167889baed76acf5bb55c3ae8fc3b64d482
Fredrik Johnsson <fredrik.johnsson7@gmail.com>
2016-09-19 09:23:24 +0200

    Really basic TypeScript application

---
+++ NodejsWebAppApplication/gulpfile.js
@@ -0,0 +1,10 @@
+﻿var gulp = require('gulp');
+var ts = require('gulp-typescript');
+
+gulp.task('default', function() {
+    return gulp.src('scripts/**/*.ts')
+        .pipe(ts({
+            noImplicitAny: true
+        }))
+        .pipe(gulp.dest('app'));
+});
